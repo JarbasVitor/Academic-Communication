5a9528fec1a356dbd94a14bd2c84d1f82f2ae097
Jarbas Vitor <jarbasvitor.pereira@hotmail.com>
2022-05-25 11:59:17 -0300

    build: project started with Spring(Boot, Data, Security), Thymeleaf.

---
+++ src/main/java/br/com/academic/communication/AcademicCommunicationApplication.java
@@ -0,0 +1,13 @@
+package br.com.academic.communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicCommunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academic/communication/AcademicCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academic.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
