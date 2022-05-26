be26ebaae9521bdfad2935c99dca54776c0dd3d7
Jarbas Vitor <jarbasvitor.pereira@hotmail.com>
2022-05-26 11:24:01 -0300

    feat: add login with JDBC database(MySql)

--- src/main/java/br/com/academic/communication/configs/WebSecurityConfig.java
+++ src/main/java/br/com/academic/communication/configs/WebSecurityConfig.java
@@ -1,19 +1,33 @@
 package br.com.academic.communication.configs;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  {
 
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http
@@ -22,6 +36,7 @@ public class WebSecurityConfig  {
 			.and()
 			.formLogin(form -> form
 					.loginPage("/login")
+					.defaultSuccessUrl("/feed", true)
 					.permitAll())
 			.logout(logout -> logout.
 					logoutUrl("/logout"));
@@ -30,14 +45,10 @@ public class WebSecurityConfig  {
 	}
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user = 
-				User.withDefaultPasswordEncoder()
-					.username("user")
-					.password("user")
-					.roles("user")
-					.build();
-		
-		return new InMemoryUserDetailsManager(user);
-	}
+	public UserDetailsManager users(AuthenticationManagerBuilder auth) throws Exception{
+
+		JdbcUserDetailsManager login = new JdbcUserDetailsManager(dataSource);
+		return login;
+	}	
+	
 }
